848cc66e0d48981621158710dfb85484aa464a9d
Kofi <kufuorkofi@gmail.com>
2022-11-29 21:31:39 +0000

    Update timeout

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 config = {
     "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 60
+    "CACHE_DEFAULT_TIMEOUT": 240
 }
 
 headers = {
@@ -33,6 +33,9 @@ def get_ethereum(time):
     else:
         json_data = {
         'sql': 'WITH current AS ( SELECT  to_address AS contract,   COUNT(DISTINCT transaction_hash) AS txns,   COUNT(DISTINCT from_address) AS active_accounts,   SUM(gas_used*gas_price/1e18) AS gas_spend,   a.created_timestamp AS created_at FROM  ethereum.transactions t   LEFT JOIN  ethereum.accounts a ON t.to_address = a.address   LEFT JOIN ethereum.tokens tok ON t.to_address = tok.contract_address LEFT JOIN ethereum.collections col ON t.to_address = col.contract_address WHERE __confirmed = true   AND t.timestamp < now()   AND t.timestamp > now() - \''+str(time)+' minutes\'::interval AND a.type = \'contract\'   AND tok.contract_address IS NULL AND col.contract_address IS NULL GROUP BY 1,5     )  , preceding AS ( SELECT  to_address AS contract,   COUNT(DISTINCT transaction_hash) AS txns,   COUNT(DISTINCT from_address) AS active_accounts,   SUM(gas_used*gas_price/1e18) AS gas_spend,   a.created_timestamp AS created_at FROM  ethereum.transactions t   LEFT JOIN  ethereum.accounts a ON t.to_address = a.address   LEFT JOIN ethereum.tokens tok ON t.to_address = tok.contract_address LEFT JOIN ethereum.collections col ON t.to_address = col.contract_address WHERE __confirmed = true   AND t.timestamp < now()  - \''+str(time)+' minutes\'::interval AND t.timestamp > now() - \''+str(time)+' minutes\'::interval  - \''+str(time)+' minutes\'::interval AND a.type = \'contract\'   AND tok.contract_address IS NULL AND col.contract_address IS NULL GROUP BY 1,5  )  SELECT c.contract, c.active_accounts, 100*(c.active_accounts - p.active_accounts)/p.active_accounts as accounts_percentage_growth, c.txns, 100*(c.txns - p.txns)/p.txns as txns_percentage_growth, c.gas_spend, 100*(c.gas_spend - p.gas_spend)/p.gas_spend as gas_spend_percentage_growth FROM current c INNER JOIN preceding p ON c.contract = p.contract WHERE p.active_accounts > 2 AND (c.active_accounts - p.active_accounts) > 0 ORDER BY 3 DESC',
+        "options": {
+        "timeout": 60
+        }
         }
         response = requests.post('https://sql.transpose.io', headers=headers, json=json_data)
         return response.json()
