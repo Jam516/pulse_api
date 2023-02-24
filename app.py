a6e8d6e43802eaf44496765d87300690e6d8b59a
Jam516 <kufuorkofi@gmail.com>
2023-02-24 21:26:04 +0000

    Add arbitrum

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 
 config = {
     "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300
+    "CACHE_DEFAULT_TIMEOUT": 600
 }
 
 headers = {
@@ -74,6 +74,38 @@ def get_polygon(time):
         }
         json_data2 = {
             'sql': ' SELECT  to_address AS contract,   COUNT(DISTINCT transaction_hash) AS txns_previous,   COUNT(DISTINCT from_address) AS active_accounts_previous,   SUM(gas_used*gas_price/1e18) AS gas_spend_previous,   a.created_timestamp AS created_at FROM  polygon.transactions t   LEFT JOIN  polygon.accounts a ON t.to_address = a.address   LEFT JOIN polygon.tokens tok ON t.to_address = tok.contract_address LEFT JOIN polygon.collections col ON t.to_address = col.contract_address WHERE __confirmed = true   AND t.timestamp < now() - \''+str(time)+' minutes\'::interval AND t.timestamp > now() - \''+str(time)+' minutes\'::interval  - \''+str(time)+' minutes\'::interval AND a.type = \'contract\'   AND tok.contract_address IS NULL AND col.contract_address IS NULL GROUP BY 1,5    ',
+            "options": {
+                "timeout": 200
+            }
+        }
+        response1 = requests.post('https://api.transpose.io/sql', headers=headers, json=json_data1)
+        response2 = requests.post('https://api.transpose.io/sql', headers=headers, json=json_data2)
+        df1 = pd.DataFrame(response1.json()['results'])
+        df2 = pd.DataFrame(response2.json()['results'])
+        df2 = df2[df2['active_accounts_previous']>5]
+        df1 = df1[df1['active_accounts']>5]
+        df = pd.merge(df1,df2,how='inner',on='contract',validate='1:1')
+        df['accounts_percentage_growth'] = df.apply(lambda x: 100*(x['active_accounts'] - x['active_accounts_previous'])/x['txns_previous'], axis =1)
+        df['txns_percentage_growth'] = df.apply(lambda x: 100*(x['txns'] - x['txns_previous'])/x['txns_previous'], axis =1)
+        df['gas_spend_percentage_growth'] = df.apply(lambda x: 100*(x['gas_spend'] - x['gas_spend_previous'])/x['txns_previous'], axis =1)
+        df = df[df['accounts_percentage_growth']>0]
+        df = df.sort_values(by=['accounts_percentage_growth'], ascending=False)
+        return df.to_json(orient='records')
+
+@app.route('/arbitrum/<time>')
+@cache.cached()
+def get_arbitrum(time):
+    if int(time) >= 120:
+        return 'Use a shorter timeslot'
+    else:
+        json_data1 = {
+            'sql': ' SELECT  to_address AS contract,   COUNT(DISTINCT transaction_hash) AS txns,   COUNT(DISTINCT from_address) AS active_accounts,   SUM(gas_used*gas_price/1e18) AS gas_spend,   a.created_timestamp AS created_at FROM  arbitrum.transactions t   LEFT JOIN  arbitrum.accounts a ON t.to_address = a.address   LEFT JOIN arbitrum.tokens tok ON t.to_address = tok.contract_address LEFT JOIN arbitrum.collections col ON t.to_address = col.contract_address WHERE __confirmed = true  AND t.timestamp > now() - \''+str(time)+' minutes\'::interval   AND a.type = \'contract\'   AND tok.contract_address IS NULL AND col.contract_address IS NULL GROUP BY 1,5    ',
+            "options": {
+                "timeout": 200
+            }
+        }
+        json_data2 = {
+            'sql': ' SELECT  to_address AS contract,   COUNT(DISTINCT transaction_hash) AS txns_previous,   COUNT(DISTINCT from_address) AS active_accounts_previous,   SUM(gas_used*gas_price/1e18) AS gas_spend_previous,   a.created_timestamp AS created_at FROM  arbitrum.transactions t   LEFT JOIN  arbitrum.accounts a ON t.to_address = a.address   LEFT JOIN arbitrum.tokens tok ON t.to_address = tok.contract_address LEFT JOIN arbitrum.collections col ON t.to_address = col.contract_address WHERE __confirmed = true   AND t.timestamp < now() - \''+str(time)+' minutes\'::interval AND t.timestamp > now() - \''+str(time)+' minutes\'::interval  - \''+str(time)+' minutes\'::interval AND a.type = \'contract\'   AND tok.contract_address IS NULL AND col.contract_address IS NULL GROUP BY 1,5    ',
             "options": {
                 "timeout": 200
             }
